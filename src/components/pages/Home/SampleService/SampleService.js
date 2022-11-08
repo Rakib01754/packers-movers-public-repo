8e33f7b2a33bd9716fcb0a249f7bcc11cee9be2e
Rakib01754 <rakibul.freelancer567@gmail.com>
2022-11-08 16:23:31 +0600

    react photoview used on home services

--- src/components/pages/Home/SampleService/SampleService.js
+++ src/components/pages/Home/SampleService/SampleService.js
@@ -13,7 +13,7 @@ const SampleService = () => {
     return (
         <>
             <h1 className='text-center text-4xl font-bold underline text-orange-600 my-6'>Our Services</h1>
-            <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
                 {
                     services.map(service => <SingleService key={service._id} service={service}></SingleService>)
                 }
--- src/components/pages/Home/SampleService/SingleService.js
+++ src/components/pages/Home/SampleService/SingleService.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
+import 'react-photo-view/dist/react-photo-view.css';
 
 const SingleService = ({ service }) => {
     const { name, details, fee, rating, picture, _id } = service
     return (
         <div className="flex justify-center">
             <div className="rounded-lg shadow-lg bg-white max-w-sm">
-                <a href="#!">
-                    <img className="rounded-t-lg h-80 w-full" src={picture} alt="" />
-                </a>
+                <PhotoProvider>
+                    <PhotoView src={picture}>
+                        <img src={picture} className="rounded-t-lg h-80 w-full" alt="" />
+                    </PhotoView>
+                </PhotoProvider>
+                {/* <img className="rounded-t-lg h-80 w-full" src={picture} alt="" /> */}
                 <div className="p-6">
                     <h5 className="text-gray-900 text-xl font-medium mb-2">{name}</h5>
                     <p className="text-gray-700 text-base mb-4">
                         {details ? details.slice(0, 100) + '...' : details}
                     </p>
+                    <div className='flex justify-between py-4 text-orange-700'>
+                        <p className='font-bold'>
+                            Fee: ${fee}
+                        </p>
+                        <p className='font-bold'>
+                            Rating: {rating}
+                        </p>
+                    </div>
                     <Link to={`/service/${_id}`}>
                         <button type="button" className="inline-block px-6 py-3 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">View Details</button>
                     </Link>

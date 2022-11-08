4e64b20d33aea2b06244cef145feb12f791ab06e
Rakib01754 <rakibul.freelancer567@gmail.com>
2022-11-08 18:38:10 +0600

    service details part updated

--- src/components/pages/Services/ServiceDetails.js
+++ src/components/pages/Services/ServiceDetails.js
@@ -2,10 +2,30 @@ import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 
 const ServiceDetails = () => {
-    const service = useLoaderData()
+    const service = useLoaderData();
+    const { name, fee, rating, details, picture } = service
     return (
         <div>
-            <h1>Service for {service.name}</h1>
+            <section className="p-6 bg-gray-100 text-white">
+                <div className="container grid gap-6 mx-auto text-center lg:grid-cols-2 xl:grid-cols-5">
+                    <div className="w-full px-2 py-16 rounded-md  md:px-8 xl:col-span-2 bg-gray-600">
+                        <h1 className="text-5xl font-extrabold text-gray-50">{name}</h1>
+                        <p className="my-3 border-white border-2 p-2 text-left">
+                            {details}
+                        </p>
+                        <div className='flex justify-between py-4 text-white text-2xl'>
+                            <p className='font-bold'>
+                                Fee: ${fee}
+                            </p>
+                            <p className='font-bold'>
+                                Rating: {rating}
+                            </p>
+                        </div>
+                    </div>
+                    <img src={picture} alt="{name}" className="object-cover w-full rounded-md xl:col-span-3 bg-gray-500 md:8 lg:mt-36" />
+                </div>
+            </section>
+
         </div>
     );
 };
